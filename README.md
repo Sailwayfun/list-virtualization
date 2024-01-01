3818fa258f1952acee256805c04f6f0d17d1b2bd
Sailwayfun <liaoleon000513@gmail.com>
2024-01-01 22:50:50 +0800

    CSS content-visibility & contain-intrinsic-size

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ This is a project exploring how to render large lists in React apps more perform
 這樣整個APP就只會渲染可視範圍內能容納的10個item，然後透過往下滾動，會繼續渲染接下來的10個item，直到list被滾動到下方沒有東西為止
 
 <img src="src/assets/no-library-demo.gif" alt="no-library" />
+
+## CSS Solution -- ```content-visibility: auto```
+```content-visibility: auto``` 是一個2020年才加入的css屬性，可以讓瀏覽器在渲染時，選擇性地跳過不在可視範圍內的元素，不去計算它們的內容和佈局，會用placeholder來取代該元素。所謂跳過渲染內容是指：元素還是會出現在DOM上，但是因為尚未進入可視範圍，其子代不會被渲染，因此可優化初次渲染時的效能。但是使用這個方法有幾個缺點：一是往下滾動時scrollbar會出現抖動，另外如果某列表中有些元素設置``visibility: hidden``或``display:none``，在offscreen(出現在可視範圍外)時，因為瀏覽器會跳過渲染其樣式，因此他們仍會出現在DOM中。
+補充：可以在list-item上另外加上```contain-intrinsic-size```，這是一個告訴瀏覽器元素不在可視範圍內時，placeholder的高度和寬度，可以稍微提高滾動時的效能。
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
-import SimpleVirtualizedList from './component/SimpleVirtualizedList';
+// import SimpleVirtualizedList from './component/SimpleVirtualizedList';
+import ContentVisibilityAutoList from './component/ContentVisibilityAutoList';
 import { useState, useEffect } from 'react';
 
 function App() {
@@ -17,27 +18,28 @@ function App() {
     setItems(newItems);
   }, []);
 
-  function renderItem(startIndex, endIndex) {
-    const renderedItems = [];
-    for (let i = startIndex; i <= endIndex; i++) {
-      const item = items[i];
-      renderedItems.push(
-        <div key={ item.id } style={ { position: "absolute", top: `${i * itemHeight}px`, width: "100%", height: `${itemHeight}px`, lineHeight: `${itemHeight}px`, textAlign: "center", border: "1px solid #ccc" } }>
-          { item.name }
-        </div>
-      );
-    }
-    return renderedItems;
-  }
+  // function renderItem(startIndex, endIndex) {
+  //   const renderedItems = [];
+  //   for (let i = startIndex; i <= endIndex; i++) {
+  //     const item = items[i];
+  //     renderedItems.push(
+  //       <div key={ item.id } style={ { position: "absolute", top: `${i * itemHeight}px`, width: "100%", height: `${itemHeight}px`, lineHeight: `${itemHeight}px`, textAlign: "center", border: "1px solid #ccc" } }>
+  //         { item.name }
+  //       </div>
+  //     );
+  //   }
+  //   return renderedItems;
+  // }
 
   return (
     <>
       <h1>List Virtualization</h1>
-      <SimpleVirtualizedList itemCount={ items.length }
+      {/* <SimpleVirtualizedList itemCount={ items.length }
         itemHeight={ itemHeight }
         windowHeight={ 500 }
         renderItem={ renderItem }
-      />
+      /> */}
+      <ContentVisibilityAutoList items={ items } itemHeight={ itemHeight } windowHeight={ 500 } />
     </>
   );
 }
