a6a8da83723000fb5a98cb8e96f80d22e0c51f60
Sailwayfun <liaoleon000513@gmail.com>
2024-01-01 21:53:59 +0800

    Build a no-library demo

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # List Virtualization
 
 This is a project exploring how to render large lists in React apps more performantly.
+
+## No Library Demo
+我們假設list中每個item的高度都是相同的，那麽list的高度就會是list height * list count，這個總高度稱為inner height。另外，需要一個固定高度的window來渲染可視範圍內的list items，這個高度稱為window height。list在window可視範圍上方的高度稱為scrollTop。
+
+建立一個SimpleVirtualizedList元件，裡面分成兩層，外層稱為scroll，用來監聽往下滾動的事件(scroll)，內層為inner，是一個position: relative的容器，高度就是innerHeight，裡面的list item用絕對定位定在容器上方，每個item都設一個相對於window的top定位。
+
+這樣整個APP就只會渲染可視範圍內能容納的10個item，然後透過往下滾動，會繼續渲染接下來的10個item，直到list被滾動到下方沒有東西為止
+
+<img src="src/assets/no-library-demo.gif" alt="no-library" />
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
 import './App.css';
+import SimpleVirtualizedList from './component/SimpleVirtualizedList';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const ITEM_COUNT = 2000;///先寫死資料數為2000筆
+  const itemHeight = 50;
+
+  useEffect(() => {
+    const newItems = new Array(ITEM_COUNT).fill(true).map((_, index) => {
+      return {
+        id: index,
+        name: `Item ${index}`,
+      };
+    });///先寫死資料數為2000筆
+    setItems(newItems);
+  }, []);
+
+  function renderItem(startIndex, endIndex) {
+    const renderedItems = [];
+    for (let i = startIndex; i <= endIndex; i++) {
+      const item = items[i];
+      renderedItems.push(
+        <div key={ item.id } style={ { position: "absolute", top: `${i * itemHeight}px`, width: "100%", height: `${itemHeight}px`, lineHeight: `${itemHeight}px`, textAlign: "center", border: "1px solid #ccc" } }>
+          { item.name }
+        </div>
+      );
+    }
+    return renderedItems;
+  }
 
   return (
     <>
       <h1>List Virtualization</h1>
+      <SimpleVirtualizedList itemCount={ items.length }
+        itemHeight={ itemHeight }
+        windowHeight={ 500 }
+        renderItem={ renderItem }
+      />
     </>
   );
 }
