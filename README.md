64c97810eebd67bfb4f7154cdb57e26934ffc501
Sailwayfun <liaoleon000513@gmail.com>
2024-01-01 20:15:55 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# List Virtualization
+
+This is a project exploring how to render large lists in React apps more performantly.
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <h1>List Virtualization</h1>
+    </>
+  );
+}
+
+export default App;
