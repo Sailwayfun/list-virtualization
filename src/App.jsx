c373c9f2bd1d1ebadce75ac188a56dee40a50a26
Sailwayfun <liaoleon000513@gmail.com>
2024-01-04 15:22:28 +0800

    fetch random user api

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,33 @@ import 'react-virtualized/styles.css';
 import SimpleVirtualizedList from './component/SimpleVirtualizedList';
 // import ContentVisibilityAutoList from './component/ContentVisibilityAutoList';
 // import ReactVirtualizedList from './component/ReactVirtualizedList';
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
+import { useRandomUsers } from './hooks/useRandomUsers';
 
 function App() {
-  const [items, setItems] = useState([]);
+  // const [items, setItems] = useState([]);
   const ITEM_COUNT = 2000;///先寫死資料數為2000筆
   const itemHeight = 50;
+  const [items] = useRandomUsers(ITEM_COUNT);
 
-  useEffect(() => {
-    const newItems = new Array(ITEM_COUNT).fill(true).map((_, index) => {
-      return {
-        id: index,
-        name: `Item ${index}`,
-      };
-    });///先寫死資料數為2000筆
-    setItems(newItems);
-  }, []);
+  // useEffect(() => {
+  //   const newItems = new Array(ITEM_COUNT).fill(true).map((_, index) => {
+  //     return {
+  //       id: index,
+  //       name: `Item ${index}`,
+  //     };
+  //   });///先寫死資料數為2000筆
+  //   setItems(newItems);
+  // }, []);
 
   function renderItem(startIndex, endIndex) {
     const renderedItems = [];
     for (let i = startIndex; i <= endIndex; i++) {
       const item = items[i];
       renderedItems.push(
-        <div key={ item.id } style={ { position: "absolute", top: `${i * itemHeight}px`, width: "100%", height: `${itemHeight}px`, lineHeight: `${itemHeight}px`, textAlign: "center", border: "1px solid #ccc" } }>
-          { item.name }
+        <div key={ item.id } className="flex gap-2 justify-center items-center" style={ { position: "absolute", top: `${i * itemHeight}px`, width: "100%", height: `${itemHeight}px`, lineHeight: `${itemHeight}px`, textAlign: "center", border: "1px solid #ccc" } }>
+          <img src={ item.image } className="w-4 h-4" />
+          <p>{ item.name }</p>
         </div>
       );
     }
@@ -42,7 +45,7 @@ function App() {
         renderItem={ renderItem }
       />
       {/* <ContentVisibilityAutoList items={ items } itemHeight={ itemHeight } windowHeight={ 500 } /> */ }
-      {/* <ReactVirtualizedList items={ items } itemHeight={ itemHeight } windowHeight={ 500 } /> */}
+      {/* <ReactVirtualizedList items={ items } itemHeight={ itemHeight } windowHeight={ 500 } /> */ }
     </>
   );
 }
